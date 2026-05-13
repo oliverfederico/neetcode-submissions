4fc5e682c3180d5fcf818ac2cfe6518360c4548d
Oliver Federico <65725580+oliverfederico@users.noreply.github.com>
2026-05-13 12:26:16 +0100

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,32 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.trie = {}
+
+    def insert(self, word: str) -> None:
+        curr = self.trie
+        for c in word:
+            if c not in curr:
+                curr[c] = {}
+            curr = curr[c]
+        curr['*'] = {}
+
+
+    def search(self, word: str) -> bool:
+        curr = self.trie
+        for c in word:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return '*' in curr
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.trie
+        for c in prefix:
+            if c not in curr:
+                return False
+            curr = curr[c]
+        return True
+        
+        
