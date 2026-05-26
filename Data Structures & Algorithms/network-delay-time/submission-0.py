cac6b3adabe481262452f045567c87f94b974db1
Oliver Federico <65725580+oliverfederico@users.noreply.github.com>
2026-05-26 14:12:46 +0100

    Add: network-delay-time - submission-0

---
+++ Data Structures & Algorithms/network-delay-time/submission-0.py
@@ -0,0 +1,28 @@
+class Solution: 
+    # we always want to process the cheapest path + edge next
+    # does it matter if we take the cheapest node + path or just the cheapest path of currently cheapest node
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        deps = [[] for _ in range(n+1)]
+        for u, v, t in times:
+            deps[u].append((t, v))
+
+        mheap = deps[k].copy()
+        heapq.heapify(mheap)
+        visited = set([k])
+
+        while mheap:
+            c, u = heapq.heappop(mheap)
+            if u not in visited:
+                visited.add(u)
+                if len(visited) == n:
+                            return c
+                print("here first {}", u)
+                for t, v in deps[u]:
+                    if v not in visited:
+                        heapq.heappush(mheap, (c+t, v))
+                        print("here second {}", v)
+                        
+        
+        return -1
+            
+        
